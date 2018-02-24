9c6c9fc85d1277c5df59be79b6a429b30929398b
David198 <support+8e29786f6a064982a8dfb5d4b68b4cd5@feedback.hockeyapp.net>
2018-02-24 17:58:42 +0100

    ChangeLog reworked. Information is now provided by Json file

---
+++ Pr0gramm/Models/ChangeLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr0gramm.Models
+{
+    public class ChangeLogItem
+    {
+        public string Version { get; set; }
+        public List<Changes> Changes { get; set; }
+    }
+
+    public class Changes
+    {
+        public string Type { get; set; }
+        public string Change { get; set; }
+    }
+
+}
--- Pr0gramm/Views/WhatsNewDialog.xaml.cs
+++ Pr0gramm/Views/WhatsNewDialog.xaml.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Storage;
 using Windows.System;
 using Windows.UI.Xaml.Controls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Pr0gramm.Models;
 
 namespace Pr0gramm.Views
 {
     public sealed partial class WhatsNewDialog : ContentDialog
     {
+        public List<ChangeLogItem> ChangeLogItems { get; set; }
+
         public WhatsNewDialog()
         {
             // TODO WTS: Update the contents of this dialog every time you release a new version of the app
             InitializeComponent();
+            DataContext = this;
+            LoadChangeLog();
         }
 
+        private async void LoadChangeLog()
+        {
+            string filepath = @"Assets\ChangeLog.json";
+            StorageFolder folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFile file = await folder.GetFileAsync(filepath); 
+            var data = await FileIO.ReadTextAsync(file);
+            ChangeLogItems = JsonConvert.DeserializeObject<ChangeLogItem[]>(data, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            }).ToList();
+        }
     }
 }
