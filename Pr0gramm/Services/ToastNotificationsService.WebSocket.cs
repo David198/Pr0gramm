37b8ec73dc212980d190118bd08652ba91451dcc
David198 <support+8e29786f6a064982a8dfb5d4b68b4cd5@feedback.hockeyapp.net>
2018-02-24 15:37:50 +0100

    Add Notification for Userbanned

--- Pr0gramm/Services/ToastNotificationsService.WebSocket.cs
+++ Pr0gramm/Services/ToastNotificationsService.WebSocket.cs
@@ -58,5 +58,58 @@ namespace Pr0gramm.Services
             // And show the toast
             ShowToastNotification(toast);
         }
+
+        public void ShowToastNotificationUserBannedExeception()
+        {
+            // Create the toast content
+            var content = new ToastContent
+            {
+                // TODO WTS: Check this documentation to know more about the Launch property
+                // Documentation: https://developer.microsoft.com/en-us/windows/uwp-community-toolkit/api/microsoft_toolkit_uwp_notifications_toastcontent
+                Launch = "ToastContentActivationParams",
+
+                Visual = new ToastVisual
+                {
+                    BindingGeneric = new ToastBindingGeneric
+                    {
+                        Children =
+                        {
+                            new AdaptiveText
+                            {
+                                Text = "UserBanned_Exception_Title".GetLocalized()
+                            },
+                            new AdaptiveText
+                            {
+                                Text = "UserBanned_Exception_Text".GetLocalized()
+                            }
+                        }
+                    }
+                },
+
+                Actions = new ToastActionsCustom
+                {
+                    Buttons =
+                    {
+                        // TODO WTS: Check this documentation to know more about Toast Buttons
+                        // Documentation: https://developer.microsoft.com/en-us/windows/uwp-community-toolkit/api/microsoft_toolkit_uwp_notifications_toastbutton
+                        new ToastButton("OK", "ToastButtonActivationArguments")
+                        {
+                            ActivationType = ToastActivationType.Foreground
+                        }
+                    }
+                }
+            };
+
+            // Add the content to the toast
+            var toast = new ToastNotification(content.GetXml())
+            {
+                // TODO WTS: Get or set the unique identifier of this notification within the notification Group. Max length 16 characters.
+                // Documentation: https://docs.microsoft.com/uwp/api/windows.ui.notifications.toastnotification
+                Tag = "UserBannedNotif"
+            };
+
+            // And show the toast
+            ShowToastNotification(toast);
+        }
     }
 }
